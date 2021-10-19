64ba8f4563225069fb701e185d950b8ab41e2d09
Vishnu Kumar Rana <vishnu.rana@skilrock.com>
2021-10-19 21:47:30 +0530

    added user model

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const moment = require("moment");
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profile_pic: {
+        type: String,
+        default: "empty-avtar.jpg"
+    },
+    createdAt: {
+        type: String,
+        default: moment().format("DD/MM/YYYY") + ";" + moment().format("hh:mm:ss")
+    },
+    updatedAt: {
+        type: String,
+        default: moment().format("DD/MM/YYYY") + ";" + moment().format("hh:mm:ss")
+    }
+});
+
+//create model
+
+mongoose.model("users", UserSchema);
+
+//export model
+module.exports = mongoose.model("users");
